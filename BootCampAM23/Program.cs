c70561a07b0a878f16c02cb9ae00b9dbb47dcb49
MUKILTEO\1604355 <tygood981@gmail.com>
2023-02-06 08:55:46 -0800

    Add project files.

---
+++ BootCampAM23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BootCampAM23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("<Press Any Key to Quit>");
+            Console.ReadKey();
+        }
+    }
+}
